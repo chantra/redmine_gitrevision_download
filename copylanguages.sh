c8ba2f613677c7456d2b3fa3a1af2d9b78e08612
chantra <chantra@debuntu.org>
2010-06-12 08:34:38 +0200

    Fixing language files generation script
    Adds " around language name with - in it

--- copylanguages.sh
+++ copylanguages.sh
@@ -8,7 +8,12 @@ lang2='config/locales'
 ls -1 $redmine_dir/$lang2 | grep -v en.yml | while read line
 do
   lang=`expr "$line" : "\(.*\).yml"`
-  echo cp $lang2/en.yml $lang2/$line
-  echo cp $lang1/en.yml $lang1/$line
-  sed -i "s/^en:/${lang}:/" $lang2/$line  
+  cp $lang2/en.yml $lang2/$line
+  cp $lang1/en.yml $lang1/$line
+  if [ `expr "$lang" : "\(.*\)-\(.*\)"` ]
+  then
+    sed -i "s/^en:/\x22${lang}\x22:/" $lang2/$line  
+  else
+    sed -i "s/^en:/${lang}:/" $lang2/$line  
+  fi
 done
