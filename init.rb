1ffd7fab9291396013163c1a2142b746234e2a4b
chantra <chantra@debuntu.org>
2011-07-26 14:10:45 +0200

    Bumping to release 0.0.8

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_gitrevision_download do
   author_url 'http://www.debuntu.org'
   url 'http://redmine.debuntu.org/projects/gitrevision-download'
   description 'A plugin adding a download link to git repository browser'
-  version '0.0.7'
+  version '0.0.8'
 
   settings :default => { :gzip => 1 }, :partial => 'settings/gitrevision_download_settings'
   # This plugin adds a project module
